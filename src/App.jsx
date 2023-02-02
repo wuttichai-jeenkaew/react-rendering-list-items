1f16f081ee57ceaec3a828489bf847b2316562fd
Mind Sathirasrindh <109655871+mydjolie@users.noreply.github.com>
2023-02-02 10:10:56 +0700

    feat:setting up HH w/ vite

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <section className="movie-list-section">
+        {/* Render Movie Lists Here */}
+      </section>
+      <section className="team-list-section">
+        {/* Render Team List Here */}
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/teams.jsx
@@ -0,0 +1,33 @@
+const teams = [
+  {
+    id: "A-100",
+    name: "TechUp",
+    children: [
+      {
+        id: "A-001-1",
+        name: "Diya",
+      },
+      {
+        id: "A-001-2",
+        name: "Beatriz",
+      },
+    ],
+  },
+  {
+    id: "B-100",
+    name: "Bacon",
+    children: [],
+  },
+  {
+    id: "C-100",
+    name: "Vitamilk",
+    children: [
+      {
+        id: "C-001-1",
+        name: "Milo",
+      },
+    ],
+  },
+];
+
+export default teams;
