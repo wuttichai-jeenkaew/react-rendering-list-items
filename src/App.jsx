f00218d63a95f24f663e077937d794088225ba12
Mu Vonglao <muvonglao@gmail.com>
2023-05-25 23:59:11 +0700

    fix(App): delete mock exercise from App

--- src/App.jsx
+++ src/App.jsx
@@ -8,38 +8,10 @@ function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
-        {movies.map((movie, index) => (
-          <div key={index}>
-            <p>Title: {movie.title}</p>
-            <p>year: {movie.year}</p>
-            <p>runtime: {movie.runtime}</p>
-            <p>director: {movie.director}</p>
-            <img src={movie.image} alt={movie.title} />
-            <ul>
-              {movie.genres.map((genre, i) => (
-                <li key={i}>{genre}</li>
-              ))}
-            </ul>
-            <p>imdbRating: {movie.imdbRating}</p>
-            <p>imdbVotes: {movie.imdbVotes}</p>
-          </div>
-        ))}
+        {/* Render Movie Lists Here */}
       </section>
       <section className="team-list-section">
-        {teams.map((team, index) => (
-          <div key={index}>
-            <p>
-              {team.name} Team ({team.id})
-            </p>
-            {team.children.map((child, index) => (
-              <div key={index}>
-                <p>
-                  {child.name} Team ({child.id})
-                </p>
-              </div>
-            ))}
-          </div>
-        ))}
+        {/* Render Team List Here */}
       </section>
     </div>
   );
