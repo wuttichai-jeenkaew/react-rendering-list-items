2ced9181d541a4edfb651f37b65738532ea53b39
Thanakrit <thanakrit.than.biz@gmail.com>
2025-03-19 16:55:30 +0700

    assignment complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import "./App.css";
+import movies from "./data/movies";
+import { MovieCard } from "./components/MovieCard";
 
 function App() {
   return (
     <div className="App">
-      <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
+      <section className="flex justify-center items-center bg-[#FDFBFB]">
+        <div className="grid grid-cols-1 gap-20 p-6">
+          <h1 className="text-[40px] font-semibold flex justify-center">
+            Movie List Section
+          </h1>
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.title}
+              movie={movie}
+              className="hover:shadow-2xl transition-shadow duration-300"
+            />
+          ))}
+        </div>
       </section>
     </div>
   );
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,39 @@
+export function MovieCard({ movie, className }) {
+  return (
+    <div
+      className={`flex items-start bg-white text-[#181818] font-semibold border border-white p-4 sm:p-4 rounded-xl shadow-lg shadow-[#917C7C40] w-full sm:w-[450px] md:w-[450px] transition-transform hover:scale-105 ${className}`}
+    >
+      {/* Movie Image */}
+      <img
+        className="w-21 h-20 sm:w-[102px] sm:h-[100px] rounded-[10px] object-cover mr-4"
+        src={movie.image}
+        alt={movie.title}
+      />
+
+      {/* Movie Details */}
+      <div className="flex flex-col text-left text-lg gap-2">
+        <h2>Title: {movie.title}</h2>
+        <p>Year: {movie.year}</p>
+        <p>Runtime: {movie.runtime} Mins</p>
+
+        {/* Genres */}
+        <div className="flex flex-wrap items-center text-lg">
+          <div className="flex flex-wrap gap-2">
+            <p>Genres:</p>
+            {movie.genres.map((genre) => (
+              <span
+                key={genre}
+                className="bg-[#EAAC99] text-[#181818] text-base px-2 py-1 rounded-[10px]"
+              >
+                {genre}
+              </span>
+            ))}
+          </div>
+        </div>
+
+        <p>IMDB Ratings: {movie.imdbRating ?? "N/A"}</p>
+        <p>IMDB Votes: {movie.imdbVotes ?? "N/A"}</p>
+      </div>
+    </div>
+  );
+}
